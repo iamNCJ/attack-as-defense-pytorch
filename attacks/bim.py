e89d24786d77a7372b14b35f1ebebed29a904ff2
NCJ <me@ncj.wiki>
2022-08-16 22:53:40 +0800

    Create bim.py

---
+++ attacks/bim.py
@@ -0,0 +1,156 @@
+import numpy as np
+import torch
+from advertorch.attacks import PGDAttack
+from advertorch.attacks.utils import rand_init_delta
+from advertorch.utils import clamp, batch_l1_proj, batch_multiply, normalize_by_pnorm, clamp_by_pnorm, batch_clamp
+from torch import nn
+
+
+def perturb_iterative(xvar, yvar, predict, nb_iter, eps, eps_iter, loss_fn,
+                      delta_init=None, minimize=False, ord=np.inf,
+                      clip_min=0.0, clip_max=1.0,
+                      l1_sparsity=None):
+    """
+    Iteratively maximize the loss over the input. It is a shared method for
+    iterative attacks including IterativeGradientSign, LinfPGD, etc.
+    :param xvar: input data.
+    :param yvar: input labels.
+    :param predict: forward pass function.
+    :param nb_iter: number of iterations.
+    :param eps: maximum distortion.
+    :param eps_iter: attack step size.
+    :param loss_fn: loss function.
+    :param delta_init: (optional) tensor contains the random initialization.
+    :param minimize: (optional bool) whether to minimize or maximize the loss.
+    :param ord: (optional) the order of maximum distortion (inf or 2).
+    :param clip_min: mininum value per input dimension.
+    :param clip_max: maximum value per input dimension.
+    :param l1_sparsity: sparsity value for L1 projection.
+                  - if None, then perform regular L1 projection.
+                  - if float value, then perform sparse L1 descent from
+                    Algorithm 1 in https://arxiv.org/pdf/1904.13000v1.pdf
+    :return: tensor containing the perturbed input and the steps.
+    """
+    if delta_init is not None:
+        delta = delta_init
+    else:
+        delta = torch.zeros_like(xvar)
+
+    final_steps = torch.ones(xvar.shape[0], dtype=torch.int32, device=xvar.device) * 2000
+
+    delta.requires_grad_()
+    for ii in range(nb_iter):
+        outputs = predict(xvar + delta)
+        _, yadv = torch.max(outputs, dim=1)
+        update_mask = (yvar != yadv) & (final_steps == 2000)
+        final_steps[update_mask == 1] = ii
+        loss = loss_fn(outputs, yvar)
+        if minimize:
+            loss = -loss
+
+        loss.backward()
+        if ord == np.inf:
+            grad_sign = delta.grad.data.sign()
+            delta.data = delta.data + batch_multiply(eps_iter, grad_sign)
+            delta.data = batch_clamp(eps, delta.data)
+            delta.data = clamp(xvar.data + delta.data, clip_min, clip_max
+                               ) - xvar.data
+
+        elif ord == 2:
+            grad = delta.grad.data
+            grad = normalize_by_pnorm(grad)
+            delta.data = delta.data + batch_multiply(eps_iter, grad)
+            delta.data = clamp(xvar.data + delta.data, clip_min, clip_max
+                               ) - xvar.data
+            if eps is not None:
+                delta.data = clamp_by_pnorm(delta.data, ord, eps)
+
+        elif ord == 1:
+            grad = delta.grad.data
+            abs_grad = torch.abs(grad)
+
+            batch_size = grad.size(0)
+            view = abs_grad.view(batch_size, -1)
+            view_size = view.size(1)
+            if l1_sparsity is None:
+                vals, idx = view.topk(1)
+            else:
+                vals, idx = view.topk(
+                    int(np.round((1 - l1_sparsity) * view_size)))
+
+            out = torch.zeros_like(view).scatter_(1, idx, vals)
+            out = out.view_as(grad)
+            grad = grad.sign() * (out > 0).float()
+            grad = normalize_by_pnorm(grad, p=1)
+            delta.data = delta.data + batch_multiply(eps_iter, grad)
+
+            delta.data = batch_l1_proj(delta.data.cpu(), eps)
+            delta.data = delta.data.to(xvar.device)
+            delta.data = clamp(xvar.data + delta.data, clip_min, clip_max
+                               ) - xvar.data
+        else:
+            error = "Only ord = inf, ord = 1 and ord = 2 have been implemented"
+            raise NotImplementedError(error)
+        delta.grad.data.zero_()
+
+    x_adv = clamp(xvar + delta, clip_min, clip_max)
+    return x_adv, final_steps
+
+
+class PatchedBIM(PGDAttack):
+    """
+    Like GradientSignAttack but with several steps for each epsilon.
+    Aka Basic Iterative Attack.
+    Paper: https://arxiv.org/pdf/1611.01236.pdf
+    :param predict: forward pass function.
+    :param loss_fn: loss function.
+    :param eps: maximum distortion.
+    :param nb_iter: number of iterations.
+    :param eps_iter: attack step size.
+    :param rand_init: (optional bool) random initialization.
+    :param clip_min: mininum value per input dimension.
+    :param clip_max: maximum value per input dimension.
+    :param targeted: if the attack is targeted.
+    """
+
+    def __init__(self, predict, loss_fn=None, eps=0.1, nb_iter=10,
+                 eps_iter=0.05, clip_min=0., clip_max=1., targeted=False):
+        ord = np.inf
+        rand_init = False
+        l1_sparsity = None
+        super(PatchedBIM, self).__init__(
+            predict, loss_fn, eps, nb_iter, eps_iter, rand_init,
+            clip_min, clip_max, ord, l1_sparsity, targeted)
+
+    def perturb(self, x, y=None):
+        """
+        Given examples (x, y), returns their adversarial counterparts with
+        an attack length of eps.
+
+        :param x: input tensor.
+        :param y: label tensor.
+                  - if None and self.targeted=False, compute y as predicted
+                    labels.
+                  - if self.targeted=True, then y must be the targeted labels.
+        :return: tensor containing perturbed inputs and the final steps.
+        """
+        x, y = self._verify_and_process_inputs(x, y)
+
+        delta = torch.zeros_like(x)
+        delta = nn.Parameter(delta)
+        if self.rand_init:
+            rand_init_delta(
+                delta, x, self.ord, self.eps, self.clip_min, self.clip_max)
+            delta.data = clamp(
+                x + delta.data, min=self.clip_min, max=self.clip_max) - x
+
+        rval, final_steps = perturb_iterative(
+            x, y, self.predict, nb_iter=self.nb_iter,
+            eps=self.eps, eps_iter=self.eps_iter,
+            loss_fn=self.loss_fn, minimize=self.targeted,
+            ord=self.ord, clip_min=self.clip_min,
+            clip_max=self.clip_max, delta_init=delta,
+            l1_sparsity=self.l1_sparsity,
+        )
+
+        return rval.data, final_steps
