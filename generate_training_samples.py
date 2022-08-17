8b4c68c496f9457e2109ec0f24aa5de4708bc7c4
NCJ <me@ncj.wiki>
2022-08-17 10:20:43 +0800

    Update generate_training_samples.py

--- generate_training_samples.py
+++ generate_training_samples.py
@@ -17,8 +17,8 @@ data_loader = dm.get_data_loader(batch_size=BS, shuffle=False)
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 fmodel = fb.PyTorchModel(model, (-10, 10), device=device)
 
-judge_attack = PatchedBIM(model, 10)
-# judge_attack = PatchedBIM(model)
+# judge_attack = PatchedJSMA(model, 10)
+judge_attack = PatchedBIM(model)
 
 # Adv samples
 for attack_type, attack in ATTACK_DICT.items():
